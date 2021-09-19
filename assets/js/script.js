f3ee3f7346fff9e952460bcb648bad85336ecee6
Lucy Woodman <lucyjwoodman@gmail.com>
2021-09-19 10:32:22 +0100

    feat: check answer and show output to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     for (const button of buttons) {
         button.addEventListener('click', function () {
-            if (this.getAttribute('data-type') === 'Submit') {
-                alert('You clicked Submit!');
+            if (this.getAttribute('data-type') === 'submit') {
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -32,12 +32,26 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
 
+/**
+ * Checks the answer against the first element in 
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert('Hey! You got it right! :D');
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}.`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
@@ -53,7 +67,7 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else {
         alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
 
